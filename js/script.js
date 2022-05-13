8b40b1188325596036bc36d4563f480f8484b26a
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-13 15:21:51 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -8,10 +8,19 @@ const app = new Vue(
     {
         el: "#root",
         data: {
-
+            rndEmails: [],
+            mail: '',
         },
-        methods: {
-
+        created() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    const email = resp.data.response;
+                    console.log(email);
+                    this.mail = email;
+                    this.rndEmails.push(this.mail); 
+                })
+            }
         },
     }
 )
